6a75c9ecad341d798df79e81fa96e741c60d24a8
Vadim Kuznetskikh <cyberwade@gmail.com>
2026-01-11 17:37:10 +0100

    first commit

---
+++ src/main/java/org/example/demo/SpringConfluentCloudApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfluentCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfluentCloudApplication.class, args);
+    }
+
+}
